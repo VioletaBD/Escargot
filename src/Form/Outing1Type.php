fe45c941e6e3168a2c58dbf3d85071f7240e7ef9
Violeta Doncheva <violeta.doncheva90@gmail.com>
2023-02-02 23:35:11 +0100

    updats done on form

--- src/Form/Outing1Type.php
+++ src/Form/Outing1Type.php
@@ -4,21 +4,37 @@ namespace App\Form;
 
 use App\Entity\Outing;
 use Symfony\Component\Form\AbstractType;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class Outing1Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('description')
-            ->add('outingName')
-            ->add('outingFile')
-            ->add('updatedAt')
-            ->add('dateTime')
-            ->add('inscription')
-        ;
+            ->add('outingFile', VichFileType::class, [
+                'label' => 'Image',
+                'required'      => true,
+                'allow_delete'  => true, // not mandatory, default is true
+                'download_uri' => true, // not mandatory, default is true
+            ])
+            ->add('description', TextType::class, [
+                'label' => 'Description'
+            ])
+
+            ->add('dateTime', null, [
+                'label' => 'timedate'
+            ])
+            ->add('inscription', null, )
+
+
+
+            ->add('dateTime', DateTimeType::class, [
+                'dateTime' => 'Date'
+            ]);;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
