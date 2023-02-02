4ab126fc4adcb266e5176ba30f6d1a5fda56fe41
Violeta Doncheva <violeta.doncheva90@gmail.com>
2023-02-02 21:30:59 +0100

    login updats done

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,5 +24,12 @@ class LoginController extends AbstractController
                 'error'         => $error,
             ]
         );
+    }
+
+    #[Route('/deconnexion', name: 'app_logout', methods: ['GET'])]
+    public function logout(): void
+    {
+        // controller can be blank: it will never be called!
+        throw new Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
