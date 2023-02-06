bcd6404265b911ae62908ebd06fb5d0f2a45ad5a
Violeta Doncheva <violeta.doncheva90@gmail.com>
2023-02-06 09:34:14 +0100

    updates done

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -10,26 +10,26 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class InscriptionController extends AbstractController
 {
-    #[Route('/inscription', name: 'app_inscription')]
-    public function index(AuthenticationUtils $authenticationUtils): Response
-    {
-        // get the inscription error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-        return $this->render(
-            'inscription/inscription.html.twig',
-            [
-                'last_username' => $lastUsername,
-                'error'         => $error,
-            ]
-        );
-    }
+    // #[Route('/inscription', name: 'app_inscription')]
+    // public function index(AuthenticationUtils $authenticationUtils): Response
+    // {
+    //     // get the inscription error if there is one
+    //     $error = $authenticationUtils->getLastAuthenticationError();
+    //     // last username entered by the user
+    //     $lastUsername = $authenticationUtils->getLastUsername();
+    //     return $this->render(
+    //         'inscription/inscription.html.twig',
+    //         [
+    //             'last_username' => $lastUsername,
+    //             'error'         => $error,
+    //         ]
+    //     );
+    // }
 
-    #[Route('/deconnexion', name: 'app_logout', methods: ['GET'])]
-    public function logout(): void
-    {
-        // controller can be blank: it will never be called!
-        throw new Exception('Don\'t forget to activate logout in security.yaml');
-    }
+    // #[Route('/deconnexion', name: 'app_logout', methods: ['GET'])]
+    // public function logout(): void
+    // {
+    //     // controller can be blank: it will never be called!
+    //     throw new Exception('Don\'t forget to activate logout in security.yaml');
+    // }
 }
